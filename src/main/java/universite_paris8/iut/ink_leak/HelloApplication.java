ab2718f30b33dbafe272aa7edeed715bd295034e
chikage <lucas.benjebara9@gmail.com>
2024-05-06 23:26:40 +0200

    création d'un cercle qui se mouvois pour simuler le joueur

--- src/main/java/universite_paris8/iut/ink_leak/HelloApplication.java
+++ src/main/java/universite_paris8/iut/ink_leak/HelloApplication.java
@@ -3,18 +3,36 @@ package universite_paris8.iut.ink_leak;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Pane root = fxmlLoader.load();
+
+        Pane PlayerID = (Pane) root.lookup("#PlayerID");
+
+        Circle circle = new Circle();
+        circle.setCenterX(300.0f);
+        circle.setCenterY(135.0f);
+        circle.setRadius(100.0f);
+        circle.setId("LePlayer");
+
+        PlayerID.getChildren().add(circle);
+
+        Scene scene = new Scene(root, 620, 840);
+        stage.setTitle("Heldlo!d!!!!!!");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {

7a6dcb385108361d80af6cfdd2303dc4dcff3f4b
Chikage <lucas.benjebara9@gmail.com>
2024-05-07 15:01:56 +0200

    transfer de la gestion du player dans le controller

--- src/main/java/universite_paris8/iut/ink_leak/HelloController.java
+++ src/main/java/universite_paris8/iut/ink_leak/HelloController.java
@@ -1,40 +1,65 @@
 package universite_paris8.iut.ink_leak;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import universite_paris8.iut.ink_leak.Player.Character;
 
-public class HelloController  {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class  HelloController implements Initializable {
+    public static Character character;
     @FXML
-    private Label welcomeText;
+    public  BorderPane toutenhaut;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        character = new Character("LePlayer", 100, 50, 20, 50);
 
+        Pane PlayerID = (Pane) toutenhaut.lookup("#PlayerID");
+
+        Circle circle = new Circle();
+        circle.setCenterX(0);
+        circle.setCenterY(0);
+        circle.setRadius(character.getSize());
+        circle.setId(character.getName());
+
+        PlayerID.getChildren().add(circle);
+    }
     @FXML
     private Pane PlayerID;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavsaFX Application!");
+    public static int getCharacterSpeed() {
+        return character.getCharacterSpeed();
     }
-    
     private int PlayerSpeed;
     @FXML
     protected void moove() {
 
         Circle circle = (Circle) PlayerID.lookup("#LePlayer");
-        PlayerSpeed = HelloApplication.character.getCharacterSpeed();
+        PlayerSpeed = getCharacterSpeed();
 
         PlayerID.setOnKeyPressed(e -> {
 
             if (e.getCode() == KeyCode.UP) {
-                circle.setCenterY(circle.getCenterY() - PlayerSpeed);
+                circle.setTranslateY(circle.getTranslateY() - PlayerSpeed);
             } else if (e.getCode() == KeyCode.DOWN) {
-                circle.setCenterY(circle.getCenterY() + PlayerSpeed);
+                circle.setTranslateY(circle.getTranslateY() + PlayerSpeed);
+
             } else if (e.getCode() == KeyCode.LEFT) {
-                circle.setCenterX(circle.getCenterX() - PlayerSpeed);
+                circle.setTranslateX(circle.getTranslateX() - PlayerSpeed);
+
             } else if (e.getCode() == KeyCode.RIGHT) {
-                circle.setCenterX(circle.getCenterX() + PlayerSpeed);
+                circle.setTranslateX(circle.getTranslateX() + PlayerSpeed);
+
             }
         });
     }
+
+
 }
--- src/main/java/universite_paris8/iut/ink_leak/Main.java
+++ src/main/java/universite_paris8/iut/ink_leak/Main.java
@@ -1,5 +1,4 @@
 package universite_paris8.iut.ink_leak;
-import javafx.fxml.FXML;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,35 +9,25 @@ import universite_paris8.iut.ink_leak.Player.Character;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
-    public static Character character;
+    public static Pane root;
 
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Pane root = fxmlLoader.load();
-
-        character = new Character("LePlayer", 100, 50, 20, 50);
-
-        Pane PlayerID = (Pane) root.lookup("#PlayerID");
-
-        Circle circle = new Circle();
-        circle.setCenterX(0);
-        circle.setCenterY(0);
-        circle.setRadius(character.getSize());
-        circle.setId(character.getName());
-
-        PlayerID.getChildren().add(circle);
-
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        root = fxmlLoader.load();
         Scene scene = new Scene(root, 1020, 640);
-
         stage.setTitle("Les péripéties de Toad Vert");
         stage.setScene(scene);
         stage.show();
 
 
+    }
+
+    public static Pane getRoot() {
+        return root;
     }
 
     public static void main(String[] args) {

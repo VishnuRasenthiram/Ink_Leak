6c2f6e67dbf375926de276a35e95eb5917ad5111
Simao <simaoneves44@gmail.com>
2024-05-28 15:19:02 +0200

    peaufinements de texte

--- src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Joueur.java
+++ src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Joueur.java
@@ -89,14 +89,14 @@ public class Joueur extends Entité{
     public void prendre_degat(int degat) {
             if (this.vie_entiteProperty.getValue() - degat < 0) { this.vie_entiteProperty.setValue(0); }
             if (System.currentTimeMillis() - dernier_degat >= invincibilite) {
-                this.vie_entiteProperty.setValue(this.vie_entiteProperty.getValue() - degat);
+                this.setVie_entiteProperty(this.vie_entiteProperty.getValue() - degat);
                 this.dernier_degat = System.currentTimeMillis();
                 System.out.println(this.vie_entiteProperty.getValue());
             }
         }
 
         public void gagner_vie(int nb_vie_gagnee) {
-            if (this.vie_entiteProperty.getValue() + nb_vie_gagnee > 6) { this.vie_entiteProperty.setValue(6); }
+            if (this.getVie_entiteProperty().getValue() + nb_vie_gagnee > 6) { this.vie_entiteProperty.setValue(6); }
             else this.setVie_entiteProperty(vie_entiteProperty.getValue() + nb_vie_gagnee);
         }
 

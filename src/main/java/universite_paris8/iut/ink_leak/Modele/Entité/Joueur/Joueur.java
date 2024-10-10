327e9e446bf85fc4a4394ab4690e3b8b2c53ee9b
Chikage <lucas.benjebara9@gmail.com>
2024-10-10 16:23:45 +0200

    Refactorisation de la gestion des déplacements pour réduire la duplication

--- src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Joueur/Joueur.java
+++ src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Joueur/Joueur.java
@@ -1,19 +1,16 @@
 package universite_paris8.iut.ink_leak.Modele.Entité.Joueur;
-import javafx.animation.PauseTransition;
+
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.util.Duration;
 import universite_paris8.iut.ink_leak.Modele.Entité.Entité;
 import universite_paris8.iut.ink_leak.Modele.Entité.Pouvoirs.*;
 import universite_paris8.iut.ink_leak.Modele.Environnement;
-import universite_paris8.iut.ink_leak.Modele.Generateurs.GenerateurEnnemis;
-import universite_paris8.iut.ink_leak.Modele.Generateurs.GenerateurMurs;
-import universite_paris8.iut.ink_leak.Modele.Map;
-import javafx.animation.KeyFrame;
-import javafx.util.Duration;
 
 public class Joueur extends Entité {
 
@@ -26,12 +23,9 @@ public class Joueur extends Entité {
     private IntegerProperty oppacitéProperty;
 
 
-
-
-
     public Joueur(String nom_joueur, Environnement environnement) {
-        super(nom_joueur,  6, 1, 30, 32,1,1000,environnement);
-        this.listePouvoirs= FXCollections.observableArrayList();
+        super(nom_joueur, 6, 1, 30, 32, 1, 1000, environnement);
+        this.listePouvoirs = FXCollections.observableArrayList();
         this.bougable = true;
         this.oppacitéProperty = new SimpleIntegerProperty(1);
 
@@ -39,28 +33,24 @@ public class Joueur extends Entité {
     }
 
     //Change l'indice du pouvoir en cours et permet de revenir à 0 si on dépasse le nombre de pouvoir débloqué
-    public void setPouvoir(int a){
-            if(a>0){
-                if(getIndicePouvoirEnCours()+1>getListePouvoirs().size()-1){
+    public void setPouvoir(int a) {
+        if (a > 0) {
+            if (getIndicePouvoirEnCours() + 1 > getListePouvoirs().size() - 1) {
+                setIndicePouvoirEnCours(0);
+            } else {
+                setIndicePouvoirEnCours(getIndicePouvoirEnCours() + 1);
+            }
+        } else {
+            if (getIndicePouvoirEnCours() - 1 < 0) {
+                if (getListePouvoirs().isEmpty()) {
                     setIndicePouvoirEnCours(0);
+                } else {
+                    setIndicePouvoirEnCours(getListePouvoirs().size() - 1);
                 }
-                else {
-                    setIndicePouvoirEnCours(getIndicePouvoirEnCours()+1);
-                }
+            } else {
+                setIndicePouvoirEnCours(getIndicePouvoirEnCours() - 1);
             }
-            else {
-                if(getIndicePouvoirEnCours()-1<0){
-                    if(getListePouvoirs().isEmpty()){
-                        setIndicePouvoirEnCours(0);
-                    }
-                    else {
-                        setIndicePouvoirEnCours(getListePouvoirs().size()-1);
-                    }
-                }
-                else {
-                    setIndicePouvoirEnCours(getIndicePouvoirEnCours()-1);
-                }
-            }
+        }
 
     }
 
@@ -71,180 +61,98 @@ public class Joueur extends Entité {
         getAttaqueDeBase().déplacement(getOrientation());
     }
 
-    public void attaqueAvecPouvoir(){
-        int indice=getIndicePouvoirEnCours();
-        if(listePouvoirs.get(indice) instanceof Bulle){
+    public void attaqueAvecPouvoir() {
+        int indice = getIndicePouvoirEnCours();
+        if (listePouvoirs.get(indice) instanceof Bulle) {
             getBulle().déplacement(getOrientation());
-        }
-        else if (listePouvoirs.get(indice) instanceof Poing) {
+        } else if (listePouvoirs.get(indice) instanceof Poing) {
             getPoing().déplacement(getOrientation());
-        } else if (listePouvoirs.get(indice) instanceof  Langue) {
+        } else if (listePouvoirs.get(indice) instanceof Langue) {
             getLangue().déplacement(getOrientation());
         }
     }
 
 
-    //Déplacement du personnage avec un timeline pour avoir de la fluidité dans les mouvements
     @Override
     public void déplacement(String direction) {
+        double vitesseJoueur = getVitesse_entite() - 0.2;
+        setMovementState(Joueur.MovementState.WALK);
 
-            double vitesseJoueur = super.getVitesse_entite()-0.2;
-            setMovementState(Joueur.MovementState.WALK);
+        if (timeline != null) return;
 
-            if (timeline != null) return;
+        double deltaX = 0, deltaY = 0;
+        switch (direction) {
+            case "N":
+                deltaY = -vitesseJoueur;
+                break;
+            case "S":
+                deltaY = vitesseJoueur;
+                break;
+            case "O":
+                deltaX = -vitesseJoueur;
+                break;
+            case "E":
+                deltaX = vitesseJoueur;
+                break;
+            default:
+                return;
+        }
 
-            timeline = new Timeline(new KeyFrame(Duration.millis(5), event -> {
-                Platform.runLater(() -> {
-                    if (!getBougable()) return;
+        double finalDeltaX = deltaX;
+        double finalDeltaY = deltaY;
+        timeline = new Timeline(new KeyFrame(Duration.millis(5), event -> {
+            Platform.runLater(() -> {
+                if (!getBougable()) return;
 
-                    double x = super.getPosX();
-                    double y = super.getPosY();
-                    switch (direction) {
-                        case "N":
-                            if (super.peutAller(x, y - vitesseJoueur, super.getMap())) {
-                                if (super.verifierInteractionEnFace(x,y - vitesseJoueur) == 1) {
-                                    Timeline timeline = new Timeline();
-                                    setBougable(false);
-                                    for (int i = 0; i < 2000; i += 2) {
-                                        int dx = i/2;
-                                        KeyFrame keyFrame = new KeyFrame(Duration.millis(i), e -> {
-                                            if (super.peutAller(x, y - dx, super.getMap())){
-                                                if (dx > 32){
-                                                    if (super.verifierInteractionEnFace(x, y-dx) == 0) {
-                                                        stopDeplacement(timeline);
-                                                    }
-                                                }
-                                                super.setPosYProperty(y - dx);
-                                            } else {
-                                                stopDeplacement(timeline);
-                                            }
+                double x = getPosX();
+                double y = getPosY();
+                double newX = x + finalDeltaX;
+                double newY = y + finalDeltaY;
 
-                                        });
-                                        timeline.getKeyFrames().add(keyFrame);
-                                    }
-
-                                    timeline.play();
-                                } else{
-                                    super.setPosYProperty(y - vitesseJoueur);
-
-                                }
-                            }
-                            break;
-                        case "S":
-                            if (super.peutAller(x, y + vitesseJoueur, super.getMap())) {
-                                if (super.verifierInteractionEnFace(x,y + vitesseJoueur) == 1) {
-                                    Timeline timeline = new Timeline();
-                                    setBougable(false);
-
-                                    for (int i = 0; i < 2000; i += 2) {
-                                        int dx = i/2;
-                                        KeyFrame keyFrame = new KeyFrame(Duration.millis(i), e -> {
-                                            if (super.peutAller(x, y + dx, super.getMap())){
-                                                if (dx > 32){
-                                                    if (super.verifierInteractionEnFace(x, y + dx) == 0) {
-                                                        stopDeplacement(timeline);
-                                                    }
-                                                }
-                                                super.setPosYProperty(y + dx);
-                                            }else {
-                                                stopDeplacement(timeline);
-
-                                            }
-                                        });
-
-                                        timeline.getKeyFrames().add(keyFrame);
-                                    }
-
-                                    timeline.play();
-                                } else{
-                                    super.setPosYProperty(y + vitesseJoueur);
-
-                                }
-                            }
-                            break;
-                        case "O":
-                            if (super.peutAller(x - vitesseJoueur, y, super.getMap())) {
-                                if (super.verifierInteractionEnFace(x - vitesseJoueur, y) == 1) {
-                                    Timeline timeline = new Timeline();
-                                    setBougable(false);
-
-                                    for (int i = 0; i < 2000; i += 2) {
-                                        int dx = i/2;
-
-                                        KeyFrame keyFrame = new KeyFrame(Duration.millis(i), e -> {
-                                            if (super.peutAller(x - dx, y, super.getMap())){
-                                                if (dx > 32){
-                                                    if (super.verifierInteractionEnFace(x-dx, y) == 0) {
-                                                        stopDeplacement(timeline);
-                                                        stopDeplacement(timeline);
-                                                    }
-                                                }
-                                                super.setPosXProperty(x - dx);
-
-                                            }else {
-                                                stopDeplacement(timeline);
-
-                                            }
-                                        });
-
-                                        timeline.getKeyFrames().add(keyFrame);
-                                    }
-
-                                    timeline.play();
-                                } else{
-                                    super.setPosXProperty(x - vitesseJoueur);
-
-                                }
-                            }
-                            break;
-                        case "E":
-                            if (super.peutAller(x + vitesseJoueur, y, super.getMap())) {
-                                if (super.verifierInteractionEnFace(x + vitesseJoueur, y) == 1) {
-                                    Timeline timeline = new Timeline();
-                                    setBougable(false);
-
-                                    for (int i = 0; i < 2000; i += 2) {
-                                        int dx = i/2;
-
-                                        KeyFrame keyFrame = new KeyFrame(Duration.millis(i), e -> {
-
-                                           if (super.peutAller(x + dx, y, super.getMap())){
-                                               if (dx > 32){
-                                                   if (super.verifierInteractionEnFace(x+dx, y) == 0) {
-                                                       stopDeplacement(timeline);
-                                                   }
-                                               }
-                                               super.setPosXProperty(x + dx);
-                                            }else {
-                                                stopDeplacement(timeline);
-                                            }
-                                        });
-
-                                        timeline.getKeyFrames().add(keyFrame);
-                                    }
-
-                                    timeline.play();
-                                } else{
-                                    super.setPosXProperty(x + vitesseJoueur);
-
-                                }
-                            }
-                            break;
-
-
-                    }
-                    super.setOrientation(direction);
-                });
-            }));
-            timeline.setCycleCount(Timeline.INDEFINITE);
-            timeline.play();
-
-
+                if (peutAller(newX, newY, getMap())) {
+                    handleMovement(newX, newY, finalDeltaX, finalDeltaY);
+                }
+                setOrientation(direction);
+            });
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
+
+    private void handleMovement(double newX, double newY, double deltaX, double deltaY) {
+        if (verifierInteractionEnFace(newX, newY) == 1) {
+            Timeline interactionTimeline = new Timeline();
+            setBougable(false);
+            for (int i = 0; i < 2000; i += 2) {
+                int dx = i / 2;
+                KeyFrame keyFrame = new KeyFrame(Duration.millis(i), e -> {
+                    double tempX = getPosX() + dx * Math.signum(deltaX);
+                    double tempY = getPosY() + dx * Math.signum(deltaY);
+
+                    if (peutAller(tempX, tempY, getMap())) {
+                        if (dx > 32 && verifierInteractionEnFace(tempX, tempY) == 0) {
+                            stopDeplacement(interactionTimeline);
+                        }
+                        setPosXProperty(tempX);
+                        setPosYProperty(tempY);
+                    } else {
+                        stopDeplacement(interactionTimeline);
+                    }
+                });
+                interactionTimeline.getKeyFrames().add(keyFrame);
+            }
+            interactionTimeline.play();
+        } else {
+            setPosXProperty(newX);
+            setPosYProperty(newY);
+        }
+    }
+
     public void stopDeplacement(Timeline tl) {
         tl.stop();
         setBougable(true);
     }
+
     public void stop() {
         if (timeline != null) {
             timeline.stop();
@@ -256,50 +164,64 @@ public class Joueur extends Entité {
 
     //gains de pv et limitation à 6
     public void gagner_vie(int nb_vie_gagnee) {
-        if (this.getVie() + nb_vie_gagnee > 6) { this.setVie_entite(6); }
-        else this.setVie_entite(this.getVie() + nb_vie_gagnee);
+        if (this.getVie() + nb_vie_gagnee > 6) {
+            this.setVie_entite(6);
+        } else this.setVie_entite(this.getVie() + nb_vie_gagnee);
     }
 
-    public boolean getBougable(){
+    public boolean getBougable() {
         return bougable;
 
     }
+
     public Bulle getBulle() {
         return getEnvironnement().getBulle();
     }
-    public Poing getPoing() { return getEnvironnement().getPoing(); }
-    public Langue getLangue() { return getEnvironnement().getLangue(); }
+
+    public Poing getPoing() {
+        return getEnvironnement().getPoing();
+    }
+
+    public Langue getLangue() {
+        return getEnvironnement().getLangue();
+    }
 
     public AttaqueDeBase getAttaqueDeBase() {
         return getEnvironnement().getAttaqueDeBase();
     }
 
-    public void setBougable(boolean bougable){
+    public void setBougable(boolean bougable) {
         this.bougable = bougable;
     }
+
     public ObservableList<Pouvoirs> getListePouvoirs() {
         return listePouvoirs;
     }
 
-    public IntegerProperty getIndicePouvoirEnCoursProperty() {return indicePouvoirEnCoursProperty;}
-    public int getIndicePouvoirEnCours(){
+    public IntegerProperty getIndicePouvoirEnCoursProperty() {
+        return indicePouvoirEnCoursProperty;
+    }
+
+    public int getIndicePouvoirEnCours() {
         return indicePouvoirEnCoursProperty.getValue();
     }
 
     public void setIndicePouvoirEnCours(int pouvoirEnCours) {
         this.indicePouvoirEnCoursProperty.setValue(pouvoirEnCours);
     }
+
     public Pouvoirs getPouvoirEnCours() {
-        if(listePouvoirs.isEmpty()){
+        if (listePouvoirs.isEmpty()) {
             return null;
         }
         return listePouvoirs.get(getIndicePouvoirEnCours());
     }
-    public void ajoutPouvoir(Pouvoirs p){
+
+    public void ajoutPouvoir(Pouvoirs p) {
         listePouvoirs.add(p);
     }
 
-    public String getOrientation(){
+    public String getOrientation() {
         return super.getOrientation();
     }
 

a0b09d2fcf4929d6bc9204812ca4e6395adfe124
Simao <simaoneves44@gmail.com>
2024-10-10 14:26:37 +0200

    Suppression d'imports inutiles chez les ennemis

--- src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Ennemis/Ennemi.java
+++ src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Ennemis/Ennemi.java
@@ -3,8 +3,6 @@ package universite_paris8.iut.ink_leak.Modele.Entité.Ennemis;
 import universite_paris8.iut.ink_leak.Modele.Entité.Entité;
 import universite_paris8.iut.ink_leak.Modele.Entité.Joueur.Joueur;
 import universite_paris8.iut.ink_leak.Modele.Environnement;
-import universite_paris8.iut.ink_leak.Modele.Generateurs.GenerateurEnnemis;
-import universite_paris8.iut.ink_leak.Modele.Map;
 
 public abstract class Ennemi extends Entité {
 
--- src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Ennemis/Slime.java
+++ src/main/java/universite_paris8/iut/ink_leak/Modele/Entité/Ennemis/Slime.java
@@ -1,10 +1,6 @@
 package universite_paris8.iut.ink_leak.Modele.Entité.Ennemis;
-import universite_paris8.iut.ink_leak.Modele.Entité.Entité;
-import universite_paris8.iut.ink_leak.Modele.Entité.Joueur.Joueur;
-import universite_paris8.iut.ink_leak.Modele.Environnement;
-import universite_paris8.iut.ink_leak.Modele.Generateurs.GenerateurEnnemis;
-import universite_paris8.iut.ink_leak.Modele.Map;
 
+import universite_paris8.iut.ink_leak.Modele.Environnement;
 
 public class Slime extends Ennemi {
 
